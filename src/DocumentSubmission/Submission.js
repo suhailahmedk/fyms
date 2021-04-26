ee247c0dcd71a44f75d968faffc4b542ca0e8a00
Suhail <suhailahmed.se17@iba-suk.edu.pk>
2021-04-26 13:05:55 +0500

    DeveProjects,submission

--- src/DocumentSubmission/Submission.js
+++ src/DocumentSubmission/Submission.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
 class Submission extends React.Component {
 
@@ -9,16 +9,73 @@ class Submission extends React.Component {
 
         return (
             <div>
-                <Card>
-                    <Card.Header>Header</Card.Header>
-                    <Card.Body>
-                        <Card.Title>{variant} Card Title </Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk
-                            of the card's content.
+                <div className="row">
+                    <div className="col-1">
+
+                    </div>
+                    <div className="col-2">
+                        <Card>
+                            <Card.Header>Abstract</Card.Header>
+                            <Card.Body>
+                                <Card.Title> Card Title </Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk
+                                    of the card's content.
                         </Card.Text>
-                    </Card.Body>
-                </Card>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div className="col-2">
+
+                    </div>
+                    <div className="col-2">
+                        <Card>
+                            <Card.Header>Proposal</Card.Header>
+                            <Card.Body>
+                                <Card.Title> Card Title </Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk
+                                    of the card's content.
+                        </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-2">
+
+                    </div>
+                    <div className="col-1">
+
+                    </div>
+                    <div className="col-2">
+                        <Card>
+                            <Card.Header>Abstract</Card.Header>
+                            <Card.Body>
+                                <Card.Title> Card Title </Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk
+                                    of the card's content.
+                        </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div className="col-2">
+
+                    </div>
+                    <div className="col-2">
+                        <Card>
+                            <Card.Header>Proposal</Card.Header>
+                            <Card.Body>
+                                <Card.Title> Card Title </Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk
+                                    of the card's content.
+                        </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </div>
             </div >
 
         )
